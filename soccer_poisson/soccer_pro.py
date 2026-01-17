3fe494150479bea11dce88bda7ed35b2c78c01b2
larssl780 <lars.larsson@gmail.com>
2026-01-17 19:09:07 +0000

    Update soccer_pro.py
    Make the betfair parser more robust.

--- soccer_poisson/soccer_pro.py
+++ soccer_poisson/soccer_pro.py
@@ -1581,51 +1581,54 @@ def xpts(xgf, xga):
 def weighted_odds(text):
     
 
-	amounts = [x for x in re.findall(r'£([\d,]+)', text)]
+    amounts, numbers = [], []
+    pattern = re.compile(r'£\s*[\d,]+(?:\.\d+)?|\d+(?:\.\d+)?')
+
+    for match in pattern.finditer(text):
+        token = match.group()
+        if token.startswith('£'):
+            value = float(token.replace('£', '').replace(',', ''))
+            amounts.append(value)
+
+        else:
+            value = float(token)
+            numbers.append(value)
+    home_odds = numbers[:2]
+    away_odds = numbers[2:4]
+    draw_odds = numbers[4:6]
+
+    home_amounts = amounts[:2]
+    away_amounts = amounts[2:4]
+    draw_amounts = amounts[4:]
 
 
-	for amt in amounts:
-	    text = text.replace(amt, '')
+    home_odds_mean = 0
+    home_amount = 0
+    for i in range(len(home_odds)):
+        home_odds_mean += home_odds[i] * home_amounts[i]
+        home_amount += home_amounts[i]
 
-	numbers = [float(x) for x in re.findall(r'\d+(?:\.\d+)?', text)]
-
-	amounts = [int(x.replace('£', '').replace(',', '')) for x in amounts]
-	home_odds = numbers[:2]
-	away_odds = numbers[2:4]
-	draw_odds = numbers[4:6]
-
-	home_amounts = amounts[:2]
-	away_amounts = amounts[2:4]
-	draw_amounts = amounts[4:]
+    home_odds_mean /= home_amount
 
 
-	home_odds_mean = 0
-	home_amount = 0
-	for i in range(len(home_odds)):
-		home_odds_mean += home_odds[i] * home_amounts[i]
-		home_amount += home_amounts[i]
+    draw_odds_mean = 0
+    draw_amount = 0
+    for i in range(len(draw_odds)):
+        draw_odds_mean += draw_odds[i] * draw_amounts[i]
+        draw_amount += draw_amounts[i]
 
-	home_odds_mean /= home_amount
-
-
-	draw_odds_mean = 0
-	draw_amount = 0
-	for i in range(len(draw_odds)):
-		draw_odds_mean += draw_odds[i] * draw_amounts[i]
-		draw_amount += draw_amounts[i]
-
-	draw_odds_mean /= draw_amount
+    draw_odds_mean /= draw_amount
 
 
-	away_odds_mean = 0
-	away_amount = 0
-	for i in range(len(away_odds)):
-		away_odds_mean += away_odds[i] * away_amounts[i]
-		away_amount += away_amounts[i]
+    away_odds_mean = 0
+    away_amount = 0
+    for i in range(len(away_odds)):
+        away_odds_mean += away_odds[i] * away_amounts[i]
+        away_amount += away_amounts[i]
 
-	away_odds_mean /= away_amount
+    away_odds_mean /= away_amount
 
-	return home_odds_mean, draw_odds_mean, away_odds_mean
+    return home_odds_mean, draw_odds_mean, away_odds_mean
 def betfair_match_odds_parser(text):
-	ho, do, ao = weighted_odds(text)
-	return [('dummy', ho, do, ao)]
+    ho, do, ao = weighted_odds(text)
+    return [('dummy', ho, do, ao)]
